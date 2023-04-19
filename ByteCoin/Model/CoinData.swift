ec14810651cb96cae3076a60e8cfb1aa63ceeee0
Muhammet Bozkurt <mubozkt@gmail.com>
2023-04-20 01:09:20 +0300

    Make API request

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,9 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Muhammet BOZKURT on 20.04.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,15 +8,37 @@
 
 import Foundation
 
+
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
-    
+    let apiKey = "46D809CD-945C-46CB-A582-8D83B7F649C0"
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
-    
-    func getCoinPrice(for: currency: String) {
-        
+    func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)/\(currency)?apiKey=\(apiKey)"
+        performRequest(with: urlString)
     }
+    
+    func performRequest(with urlString: String) {
+        // create url
+        if let url = URL(string: urlString) {
+            // create urlsession
+            let session = URLSession(configuration: .default)
+            // give session a task
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    print(String(data: safeData, encoding: .utf8)!)
+                }
+            }
+            // start the task
+            task.resume()
+        }
+
+    }
+    
 }
